d0f96990b6859e85ae156acf6241d26195e80749
Davi-OS <davioliveirasanto.work@gmail.com>
2023-10-26 19:40:20 -0300

    Primeiro Commit

---
+++ Context/ProjetoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalho_POO.Models;
+
+namespace Trabalho_POO.Context
+{
+    public class ProjetoDbContext: DbContext
+    {
+        private const string connectionString = "Server=localhost;Database=ProjetoPOO;Uid=root;Pwd=Droit*18;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+        }
+        
+        public DbSet<Conta> Conta { get; set; }
+        public DbSet<Cliente_>Clientes { get; set; }
+        public DbSet<ContaAgua> ContaAgua { get; set; }
+        public DbSet<ContaLuz> ContaLuz { get; set; }
+    }
+}
